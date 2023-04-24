b539c2e7d697d16c9cca8fc8329add4c079238df
fatimamahate <>
2023-04-24 21:10:15 +0000

    Inital Commit

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('h2').addClass('underline'); // underlines all h2 elements
+    $('ul').addClass('border'); // adds border to all lists
+    $('h2').removeClass('underline'); // removes underlines all h2 elements
+    $('ul').removeClass('border'); // removes border to all lists
+});
